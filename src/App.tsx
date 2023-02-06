46d53bc40fed8c7e82ef5459526289632f92e7dc
bruno-liberatti <bruno.liberatti@b8one.com>
2023-02-06 11:43:00 -0300

    aula 2 - 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable no-const-assign */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+interface PropsVoltas {
+  voltas: number
+}
+
+interface PropsTempo {
+  tempo: number
+}
+
+interface PropsButton {
+  text: string
+  onClick: () => void
+}
+
+const MostrarVoltas: React.FC<PropsVoltas> = ({ voltas }) => {
+  return (
+    <>
+      <p>{voltas}<br/>voltas</p>
+    </>
+  )
+}
+
+const TempoVolta: React.FC<PropsTempo> = ({ tempo }) => {
+  return (
+    <>
+      <p>{tempo} tempo médio por volta</p>
+    </>
+  )
+}
+
+
+const Button: React.FC<PropsButton> = ({ text, onClick }) => <button onClick = { onClick }> { text } </button>
+
 function App() {
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [tempo, setTempo] = useState(0)
+  const [running, setRunning] = useState(false)
+
+  useEffect(() => {
+    let timer: number | null = null
+    if(running) {
+      setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    } 
+  }, [running])
+
+  const toogleRunning = () => {
+    setRunning (!running)
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MostrarVoltas voltas = {numVoltas}/>
+      <Button text='-' onClick = {decrement}/>
+      <Button text='+' onClick = {increment}/>
+      <TempoVolta tempo = {tempo}/>
+      <Button text='inicar' onClick = {toogleRunning}/>
+      <Button text='reiniciar' onClick = {increment}/>
     </div>
-  );
+  )
 }
 
 export default App;
